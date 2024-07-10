04c47b19c36d6d92326078f2db1b8fb8b7fa969f
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2024-07-10 14:50:03 +0600

    Top Banner Done and fixed some navbar issue

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const searchInput = document.getElementById("search-input");
+const searchInput1 = document.getElementById("search-input1");
 const searchIcon = document.getElementById("search-icon");
+const searchIcon1 = document.getElementById("search-icon1");
 
 searchInput.addEventListener("input", () => {
   if (searchInput.value.length > 0) {
     // console.log(searchInput.value.length);
     searchIcon.classList.add("hidden-icon");
+    searchIcon1.classList.add("hidden-icon");
   } else {
     searchIcon.classList.remove("hidden-icon");
+    searchIcon1.classList.remove("hidden-icon");
   }
 });
